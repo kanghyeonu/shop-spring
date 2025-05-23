ef8506c017dfd5c3067b53ee0e01295832838a77
KHW <51577531+kanghyeonu@users.noreply.github.com>
2025-05-23 17:06:34 +0900

    test: 회원 서비스 관련 기능 테스트 작성

--- src/test/java/shop/shop_spring/Member/MemberServiceTest.java
+++ src/test/java/shop/shop_spring/Member/MemberServiceTest.java
@@ -16,6 +16,7 @@ import static  org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -40,7 +41,7 @@ public class MemberServiceTest {
         member.setName("홍길동");
         member.setAddress("테스트시");
         member.setAddressDetail("테스트동");
-        member.setBirthDate(LocalDate.now());
+        member.setBirthDate(LocalDate.of(1998, 1, 1));
         member.setNickname("테스트");
 
         return member;
@@ -188,9 +189,10 @@ public class MemberServiceTest {
 
         when(memberRepository.findByUsername(username)).thenReturn(Optional.of(member));
         when(passwordEncoder.encode(newRawPassword)).thenReturn(encodedNewPassword);
+        when(memberRepository.save(any(Member.class))).thenReturn(member);
 
         ArgumentCaptor<Member> memberArgumentCaptor = ArgumentCaptor.forClass(Member.class);
-        when(memberRepository.save(any(Member.class))).thenReturn(member);
+
 
         // when
         memberService.updatePassword(username, newRawPassword);
@@ -206,7 +208,148 @@ public class MemberServiceTest {
         assertEquals(encodedNewPassword, capturedMember.getPassword(), "Member 객체의 비밀번호가 업데이트 되어야함");
         assertEquals(encodedNewPassword, member.getPassword(), "비밀번호 업데이트 후 기존 Member 객체도 업데이트 되어야함");
         assertEquals(member.getId(), capturedMember.getId());
+    }
 
+
+    @Test
+    void 회원_정보_모든_정보_수정(){
+        // given
+        Long existingId = 1L;
+        String existingUsername = "test@example.com";
+        String newRawPassword = "newPassword";
+        String encodedNewRawPassword = "encodedPassword";
+        LocalDate newBirthDate = LocalDate.of(1999,1, 1);
+        String newAddress = "newAddress";
+        String newAddressDetail = "newAddressDetail";
+        String newNickname = "newNickname";
+
+        Member existingMember = createTestMember();
+        existingMember.setId(existingId);
+        existingMember.setUsername(existingUsername);
+        existingMember.setPassword("oldPassword");
+
+        Member updatedMember = createTestMember();
+        updatedMember.setId(existingId);
+        updatedMember.setUsername(existingUsername);
+        updatedMember.setPassword(newRawPassword);
+        updatedMember.setBirthDate(newBirthDate);
+        updatedMember.setAddress(newAddress);
+        updatedMember.setAddressDetail(newAddressDetail);
+        updatedMember.setNickname(newNickname);
+
+        when(memberRepository.findByUsername(existingUsername)).thenReturn(Optional.of(existingMember));
+        when(passwordEncoder.encode(newRawPassword)).thenReturn(encodedNewRawPassword);
+        when(memberRepository.save(any(Member.class))).thenReturn(existingMember);
+
+        ArgumentCaptor<Member> argumentCaptor = ArgumentCaptor.forClass(Member.class);
+
+        // when
+        memberService.updateMember(updatedMember);
+
+        // then
+        verify(memberRepository, times(1)).findByUsername(existingUsername);
+        verify(passwordEncoder, times(1)).encode(newRawPassword);
+        verify(memberRepository, times(1)).save(argumentCaptor.capture());
+
+        Member capturedMember = argumentCaptor.getValue();
+        assertNotNull(capturedMember, "save()에 전달된 인자가 null임");
+        assertEquals(encodedNewRawPassword, capturedMember.getPassword(), "기존 회원의 비밀번호가 업데이트 되지 않음");
+        assertEquals(newBirthDate, capturedMember.getBirthDate(), "기존 회원의 생일이 업데이트 되지 않음");
+        assertEquals(newAddress, capturedMember.getAddress(), "기존 회원의 주소가 업데이트 되지 않음");
+        assertEquals(newAddressDetail, capturedMember.getAddressDetail(), "기존 회원의 상세 주소가 업데이트 되지 않음");
+        assertEquals(newNickname, capturedMember.getNickname(), "기존 회원의 상세 주소가 업데이트 되지 않음");
+
+        assertEquals(existingMember.getId(), capturedMember.getId());
+        assertEquals(existingMember.getUsername(), capturedMember.getUsername());
+    }
+
+    @Test
+    void 회원_정보_일부_정보_수정(){
+        // given
+        Long existingId = 1L;
+        String existingUsername = "test@example.com";
+        String newAddress = "newAddress";
+        String newAddressDetail = "newAddressDetail";
+
+        Member existingMember = createTestMember();
+        existingMember.setId(existingId);
+        existingMember.setUsername(existingUsername);
+
+        Member updatedMember = createTestMember();
+        updatedMember.setPassword(null);
+        updatedMember.setBirthDate(null);
+        updatedMember.setAddress(newAddress);
+        updatedMember.setAddressDetail(newAddressDetail);
+        updatedMember.setNickname(null);
+
+        when(memberRepository.findByUsername(existingUsername)).thenReturn(Optional.of(existingMember));
+
+        ArgumentCaptor<Member> argumentCaptor = ArgumentCaptor.forClass(Member.class);
+        when(memberRepository.save(any(Member.class))).thenReturn(existingMember);
+
+        // when
+        memberService.updateMember(updatedMember);
+
+        // then
+        verify(memberRepository, times(1)).findByUsername(existingUsername);
+        verify(memberRepository, times(1)).save(argumentCaptor.capture());
+        verify(passwordEncoder, never()).encode(anyString());
+
+        Member capturedMember = argumentCaptor.getValue();
+        assertNotNull(capturedMember);
+        // 바뀐거 체크
+        assertEquals(newAddress, capturedMember.getAddress(), "기존 회원의 주소가 업데이트되지 않음");
+        assertEquals(newAddressDetail, capturedMember.getAddressDetail(), "기존 회원의 주소가 업데이트 되지않음");
+        // 바뀌지 앟은거 체크
+        assertEquals("test", capturedMember.getPassword(), "비밀번호가 업데이트됨");
+        assertEquals("테스트", capturedMember.getNickname(), "닉네임이 업데이트됨");
+        assertEquals(LocalDate.of(1998, 1, 1), capturedMember.getBirthDate(), "생일이 업데이트됨");
+
+        assertEquals(existingMember.getId(), capturedMember.getId());
+        assertEquals(existingMember.getUsername(), capturedMember.getUsername());
+    }
+
+    @Test
+    void 회원_정보_수정_없음(){
+        // given
+        String existingUsername = "test@example.com";
+
+        Member existingMember = createTestMember();
+        existingMember.setUsername(existingUsername);
+
+        Member updatedMember = createTestMember();
+        updatedMember.setUsername(existingUsername);
+        updatedMember.setName(null);
+        updatedMember.setNickname(null);
+        updatedMember.setAddressDetail(null);
+        updatedMember.setAddress(null);
+        updatedMember.setPassword(null);
+
+        when(memberRepository.findByUsername(existingUsername)).thenReturn(Optional.of(existingMember));
+
+        ArgumentCaptor<Member> argumentCaptor = ArgumentCaptor.forClass(Member.class);
+        when(memberRepository.save(any(Member.class))).thenReturn(existingMember);
+
+        // when
+        memberService.updateMember(updatedMember);
+
+        // then
+        verify(memberRepository, times(1)).findByUsername(existingUsername);
+        verify(passwordEncoder, never()).encode(anyString());
+        verify(memberRepository, times(1)).save(argumentCaptor.capture());
+
+        Member capturedMember = argumentCaptor.getValue();
+        Member temp = createTestMember();
+        temp.setUsername(existingUsername);
+        assertNotNull(capturedMember);
+        assertEquals(temp.getPassword(), capturedMember.getPassword(), "기존 회원 비밀버호가 업데이트됨");
+        assertEquals(temp.getAddress(), capturedMember.getAddress(), "기존 회원 주소가 업데이트됨");
+        assertEquals(temp.getAddressDetail(), capturedMember.getAddressDetail(), "기존 회원 상세주소가 업데이트됨");
+        assertEquals(temp.getBirthDate(), capturedMember.getBirthDate(), "기존 회원 생일이 업데이트됨");
+        assertEquals(temp.getNickname(), capturedMember.getNickname(), "기존 회원의 닉네임이 업데이트됨");
+
+        assertEquals(existingMember.getId(), capturedMember.getId());
+        assertEquals(existingMember.getUsername(), capturedMember.getUsername());
 
     }
 }
