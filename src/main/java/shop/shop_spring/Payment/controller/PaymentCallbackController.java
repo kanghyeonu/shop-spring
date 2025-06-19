af8fa2ed476750a043d242fc2f11426857bc3d1b
KHW <51577531+kanghyeonu@users.noreply.github.com>
2025-06-19 17:32:16 +0900

    feat: 결제 콜백 컨트롤러 구현
    Payment 도메인의 PaymentCallbackController 클래스 추가
    - 결제 콜백을 받기 위한 엔드포인드 구현
    - callback 요청 데이터를 파싱하고 알맞은 callback 핸들러 호출

---
+++ src/main/java/shop/shop_spring/Payment/controller/PaymentCallbackController.java
@@ -0,0 +1,49 @@
+package shop.shop_spring.Payment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import shop.shop_spring.Order.sevice.OrderService;
+
+import java.security.PublicKey;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/payments")
+@RequiredArgsConstructor
+public class PaymentCallbackController {
+    private OrderService orderService;
+
+    @PostMapping("/mock-callback/sucess")
+    public ResponseEntity handleMockPaymentSuccessCallback(@RequestBody Map<String, Object> callbackData){
+
+         Long orderId;
+         try {
+             Object orderIdObj = callbackData.get("orderId");
+             if (orderIdObj == null){
+                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: orderId missing");
+             }
+
+             if (orderIdObj instanceof Number){
+                 orderId = ((Number) orderIdObj).longValue();
+             } else {
+                 orderId = Long.valueOf(orderIdObj.toString());
+             }
+         } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: invalid data format");
+         }
+
+         try {
+            orderService.handlePaymentSuccessCallback(orderId);
+            return ResponseEntity.status(HttpStatus.OK).body("OK");
+         } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: Internal server error");
+         }
+    }
+
+    // 실패는 생략
+}
