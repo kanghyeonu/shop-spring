f220e086c80a4be02b9a614c790c529721c6e7b7
KHW <51577531+kanghyeonu@users.noreply.github.com>
2025-07-02 10:52:02 +0900

    rename: 상품 구매 테스트 코드의 이름을 명확하게 변경

--- src/test/java/shop/shop_spring/orders/service/purchaseSingleItemTest.java
+++ src/test/java/shop/shop_spring/orders/service/purchaseSingleItemTest.java
@@ -32,7 +32,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class orderServiceSingleItemTest {
+public class purchaseSingleItemTest {
 
     @InjectMocks
     private OrderServiceImpl orderService;
