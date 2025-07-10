9081418aae9581868ad4d22c0711c9f6e34f3b59
KHW <51577531+kanghyeonu@users.noreply.github.com>
2025-07-10 15:58:21 +0900

    Refactor: Swagger API 문서를 위한 장바구니 컨트롤러 분리
    - RestController와 view 컨트롤러를 분리하여 작성
    - RestController의 각 api에 swagger 템플릿으로 api 문서 작성
    - 장바구니 API 컨트롤러에서 사용하는 DTO에 대한 @Schema 추가

--- src/main/java/shop/shop_spring/cart/dto/CartAddRequest.java
+++ src/main/java/shop/shop_spring/cart/dto/CartAddRequest.java
@@ -1,5 +1,6 @@
 package shop.shop_spring.cart.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "장바구니에 상품을 추가할 때 사용하는 요청 DTO")
 public class CartAddRequest {
     Long productId;
     Integer quantity;
--- src/main/java/shop/shop_spring/cart/dto/CartItemUpdateRequest.java
+++ src/main/java/shop/shop_spring/cart/dto/CartItemUpdateRequest.java
@@ -1,5 +1,6 @@
 package shop.shop_spring.cart.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "장바구니에 담긴 상품의 수량을 수정할 대 사용하는 DTO")
 public class CartItemUpdateRequest {
     Integer quantity;
 }
