1470e74cc6668aadf9edf6b8e94e5b92216d5a56
KHW <51577531+kanghyeonu@users.noreply.github.com>
2025-07-11 16:04:47 +0900

    refactor: 주문 관련 API 리팩토링
    - 주문 관련 뷰 컨트롤럴, API 컨트롤러 분리
    - API 컨트롤러에 API 설명 추가
    - API 컨트롤러에서 사용되는 DTO 들에 대한 설명 추가

--- src/main/java/shop/shop_spring/order/controller/OrderApiController.java
+++ src/main/java/shop/shop_spring/order/controller/OrderApiController.java
@@ -1,30 +1,34 @@
 package shop.shop_spring.order.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import shop.shop_spring.Dto.CustomApiResponse;
 import shop.shop_spring.order.Dto.CartItemOrderRequest;
-import shop.shop_spring.order.Dto.OrderDetailDto;
-import shop.shop_spring.order.Dto.OrderSummaryDto;
 import shop.shop_spring.order.Dto.SingleItemOrderRequest;
 import shop.shop_spring.order.sevice.OrderServiceImpl;
 import shop.shop_spring.payment.Dto.PaymentInitiationResponse;
 import shop.shop_spring.security.model.MyUser;
 
-import java.util.List;
-
-@Controller
+@RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
-public class OrderController {
+@Tag(name = "order API", description = "주문 관련 API(주문 조회, 주문 등)")
+public class OrderApiController {
     private final OrderServiceImpl orderService;
 
+    @Operation(summary = "단일 상품 구매", description = "단일 상품을 정해진 수량 만큼 구매")
     @PostMapping("/single-item/{productId}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "주문 시작 및 결제 요청 정보 생성"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청(상품 재고 부족, PG사 결제 이슈)"),
+    })
     public ResponseEntity placeSingleItemOrder(
             @PathVariable Long productId,
             @RequestBody SingleItemOrderRequest request,
@@ -44,7 +48,12 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @Operation(summary = "장바구니 상품 구매", description = "장바구니 내의 상품들을 정해진 수량만큼 구매")
     @PostMapping("/cart-items")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "주문 시작 및 결제 요청 정보 생성"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청(특정 상품 재고 부족, PG사 결제 이슈)"),
+    })
     public ResponseEntity placeCartItemOrder(
             @RequestBody CartItemOrderRequest request,
             Authentication auth){
@@ -62,27 +71,13 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    @GetMapping("/my-orders")
-    public String getMyOrderPage(Authentication auth, Model model){
-        MyUser member = (MyUser) auth.getPrincipal();
-        List<OrderSummaryDto> orderSummaries = orderService.getOrdersByMember(member.getId());
 
-        model.addAttribute("orderSummaries", orderSummaries);
-        return "members/my-page/orders";
-    }
-
-    @GetMapping("/{orderId}")
-    public String getOrderDetailsPage(@PathVariable("orderId") Long orderId, Authentication auth, Model model){
-        MyUser member = (MyUser) auth.getPrincipal();
-
-        OrderDetailDto orderDetailDto = orderService.getOrderDetails(member.getId(), orderId);
-
-        model.addAttribute("orderDetails", orderDetailDto);
-        return "orders/details";
-
-    }
-
+    @Operation(summary = "주문 취소", description = "배송 되기 전의 상품들을 취소")
     @PostMapping("/{orderId}/cancel")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "취소됨"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청(주문 상태가 맞지않음 or 주문 존재x)"),
+    })
     public ResponseEntity cancelOrder(@PathVariable("orderId") Long orderId, Authentication auth){
         MyUser member = (MyUser) auth.getPrincipal();
 
