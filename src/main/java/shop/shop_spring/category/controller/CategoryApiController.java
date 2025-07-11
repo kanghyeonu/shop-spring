b246dc5f9b5371d951f92ac2699d2ce58e69942b
KHW <51577531+kanghyeonu@users.noreply.github.com>
2025-07-11 14:05:42 +0900

    refactor: 카테고리 관련 컨트롤러 이름 리팩토링
    - api 기능을 수행하는 컨트롤러를 이름을 바꾸어서 명확히 표현하도록 변경
    - 이에 맞춰 FE 쪽에서도 수정된 컨트롤러 엔드포인트에 요청하도록 경로 수정

--- src/main/java/shop/shop_spring/category/controller/CategoryApiController.java
+++ src/main/java/shop/shop_spring/category/controller/CategoryApiController.java
@@ -1,29 +1,33 @@
 package shop.shop_spring.category.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import shop.shop_spring.category.service.CategoryService;
 import shop.shop_spring.category.domain.Category;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/categories")
+@RequestMapping("/api/categories")
 @RequiredArgsConstructor
-public class CategoryController {
+@Tag(name = "Categories API", description = "카테고리 관련 API")
+public class CategoryApiController {
 
     private final CategoryService categoryService;
 
+    @Operation(summary = "상품 상위 카테고리 조회", description = "최상위 카테고리를 조회")
     @GetMapping("/parents")
     @ResponseBody
     public List<Category> getParentCategories() {
         return categoryService.findByParentIsNull();
     }
 
+    @Operation(summary = "상품 하위 카테고리 조회", description = "하위 카테고리를 조회")
     @GetMapping("/children")
     @ResponseBody
     public List<Category> getChildCategories(@RequestParam Long parentId){
